045a6e0252c92569f959df28e36895c13736a379
Felipe Espinoza <espinozafelipe2004@gmail.com>
2024-06-25 18:33:49 -0400

    Version 1

---
+++ Felipe Espinoza.py
@@ -0,0 +1,8 @@
+#1.	Asignar saldos aleatorios: Generar saldos aleatorios para 10 clientes.
+#2.	Clasificar saldos: Mostrar los saldos clasificados en tres rangos específicos.
+#3.	Ver estadísticas: Calcular y mostrar estadísticas avanzadas sobre los saldos.
+   #a.	Saldo más alto
+   #b.	Saldo más bajo
+   #c.	Saldo promedio
+   #d.	Media geométrica.
+#4.	Reporte de saldos: Generar un reporte detallado de saldos con deducciones por diferentes conceptos y saldo neto, y exportar a un archivo CSV

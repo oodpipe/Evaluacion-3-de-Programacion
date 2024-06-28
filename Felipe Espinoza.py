68fa953c719c3063949e9e9078fc0568c86ba774
Felipe Espinoza <espinozafelipe2004@gmail.com>
2024-06-28 16:56:58 -0400

    Genero saldos aleatorios a los 10 Clientes

--- Felipe Espinoza.py
+++ Felipe Espinoza.py
@@ -6,3 +6,13 @@
    #c.	Saldo promedio
    #d.	Media geométrica.
 #4.	Reporte de saldos: Generar un reporte detallado de saldos con deducciones por diferentes conceptos y saldo neto, y exportar a un archivo CSV
+
+import random
+
+#aca se genera saldos al azar 
+def generar_saldos_aleatorios():
+    saldos = []
+    for _ in range(10):
+        saldo = round(random.uniform(1000, 10000), 2)  # Número aleatorio entre 1000 y 10000, Ocupo el '2' para que redonde lso saldos aleatorios para q sea mas manejable numeros
+        saldos.append(saldo)
+    return saldos

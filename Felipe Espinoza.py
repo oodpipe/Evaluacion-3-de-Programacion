01319587845b9d4b3f8acaf811f6ac991d092730
Felipe Espinoza <espinozafelipe2004@gmail.com>
2024-06-28 17:13:45 -0400

    Genero cambio en 'saldos al azar' y clasifico saldos en 3 rangos distintos

--- Felipe Espinoza.py
+++ Felipe Espinoza.py
@@ -1,4 +1,4 @@
-#1.	Asignar saldos aleatorios: Generar saldos aleatorios para 10 clientes.
+#1.	Asignar saldos aleatorios: Generar saldos aleatorios para 10 clientes. '''''OK''''''
 #2.	Clasificar saldos: Mostrar los saldos clasificados en tres rangos específicos.
 #3.	Ver estadísticas: Calcular y mostrar estadísticas avanzadas sobre los saldos.
    #a.	Saldo más alto
@@ -12,7 +12,22 @@ import random
 #aca se genera saldos al azar 
 def generar_saldos_aleatorios():
     saldos = []
-    for _ in range(10):
+    for cliente in range(10):
         saldo = round(random.uniform(1000, 10000), 2)  # Número aleatorio entre 1000 y 10000, Ocupo el '2' para que redonde lso saldos aleatorios para q sea mas manejable numeros
         saldos.append(saldo)
     return saldos
+
+def clasificar_saldos(saldos):
+    bajos = []
+    medios = []
+    altos = []
+    
+    for saldo in saldos:
+        if saldo < 3000:
+            bajos.append(saldo) #lista de los de bajoo
+        elif saldo < 7000:
+            medios.append(saldo) #lista de lso de medios
+        else:
+            altos.append(saldo) #lista de lso de altoss 7k o mas
+    
+    return bajos, medios, altos

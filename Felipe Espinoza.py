643c66291103cca2c03c2d986b37b40ce86446e0
Felipe Espinoza <espinozafelipe2004@gmail.com>
2024-06-28 18:30:07 -0400

    se agrega 'media geometrica' ocupe statistics

--- Felipe Espinoza.py
+++ Felipe Espinoza.py
@@ -1,9 +1,9 @@
-#1.	Asignar saldos aleatorios: Generar saldos aleatorios para 10 clientes.
-#2.	Clasificar saldos: Mostrar los saldos clasificados en tres rangos específicos.
+#1.	Asignar saldos aleatorios: Generar saldos aleatorios para 10 clientes. '''''OK'''''
+#2.	Clasificar saldos: Mostrar los saldos clasificados en tres rangos específicos. '''''OK''''''
 #3.	Ver estadísticas: Calcular y mostrar estadísticas avanzadas sobre los saldos.
-   #a.	Saldo más alto
-   #b.	Saldo más bajo
-   #c.	Saldo promedio
+   #a.	Saldo más alto '''ok'''
+   #b.	Saldo más bajo''' OK''''
+   #c.	Saldo promedio '''OK'''''
    #d.	Media geométrica.
 #4.	Reporte de saldos: Generar un reporte detallado de saldos con deducciones por diferentes conceptos y saldo neto, y exportar a un archivo CSV
 
@@ -53,4 +53,10 @@ def calcular_promedio(lista): #funcion para sacar el promedio de la lista ocupo 
     suma = sum(lista)
     promedio = suma / len(lista)
     return promedio
+
+import statistics
+
+def calcular_media_geometrica(lista): #ocupe statistics proporciona una función llamada geometric_mean que realiza este cálculo de manera eficiente, sin pérdida de precisión
+    media_geometrica = statistics.geometric_mean(lista)
+    return media_geometrica
 
